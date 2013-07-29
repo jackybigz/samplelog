1abfdd6395b5ba220afeb48154bc1f09ffff6fb8
Jack Hampson <jackybigz@gmail.com>
2013-07-29 15:52:52 +0100

    Styled a few things up, remade functionality and created a new show and form .erb page

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,5 @@
 class Request < ActiveRecord::Base
+	validates_presence_of :request_smd_number, :request_date, :request_date_required, 
+	:request_required_for, :request_brief_description, :request_comments,
+	:request_supplier
 end
